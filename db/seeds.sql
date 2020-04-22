be7eca144c276911c14541dd3e9b2ffb7feba337
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-04-21 20:12:20 -0400

    added 3 burger inserts

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,10 @@
 /*Write insert queries to populate burger table*/
 
 INSERT INTO burgers (burgername) 
-VALUES ('Whopper');
+VALUES ('Jack Daniels Burger');
+
+INSERT INTO burgers (burgername) 
+VALUES ('Gordan Ramsey Burger');
+
+INSERT INTO burgers (burgername) 
+VALUES ('Bohemian Radishy Burger');
