f1289993c585e7e55e386c5616e81ae7486e2ef8
Marian Akiwumi <mfakiwumi1992@yahoo.com>
2020-04-26 17:22:45 -0400

    added design scripts and fixed model

--- models/burger.js
+++ models/burger.js
@@ -5,28 +5,23 @@ const orm = require("../config/orms");
 const burger = {
     // Display all burgers in the db
     selectAll: function(cb) {
-        orm.selectAll("burgers", function(res) {
+        orm.selectAll(function(res) {
             cb(res);
         });
     },
     // Add a new burger to the db
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
+    insertOne: function(burgName, cb) {
+        orm.insertOne(burgName,  function(res) {
             cb(res);
         });
     },
     // Change the devoured status to true
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
-            cb(res);
-        });
-    },
-    // Delete a burger from the db
-    deleteOne: function(condition, cb) {
-        orm.deleteOne("burgers", condition, function(res) {
+    updateOne: function(burgStatus,burgerId, cb) {
+        orm.updateOne(burgStatus,burgerId, function(res) {
             cb(res);
         });
     }
+  
 };
 
 module.exports = burger;
